f7ef2788ccdfb0c298f11c812190a2ceb3112096
Jakub Boros <xboros1@mail.muni.cz>
2022-03-15 15:11:10 +0100

    Modernized texture loading code.

--- src/main.c
+++ src/main.c
@@ -453,17 +453,8 @@ static unsigned loadTexture(const char *path)
     glTextureParameteri(texture, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(
-        GL_TEXTURE_2D,
-        0,
-        GL_RGBA,
-        width,
-        height,
-        0,
-        GL_RGBA,
-        GL_UNSIGNED_BYTE,
-        image
-    );
+    glTextureStorage2D(texture, log2(width), GL_RGBA8, width, height);
+    glTextureSubImage2D(texture, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, image);
 
     glGenerateTextureMipmap(texture);
 
