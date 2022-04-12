d48889badb9eed3f8e2ff3510b75a4b7426aa7b5
Jakub Boros <xboros1@mail.muni.cz>
2022-04-12 15:05:09 +0200

    And vertex shader.

--- shaders/text_vertex.glsl
+++ shaders/text_vertex.glsl
@@ -16,14 +16,24 @@ layout(location = 0) out vec2 o_coords;
 
 layout(location = 0) uniform ivec2 u_winRes;
 layout(location = 1) uniform ivec2 u_position;
-layout(location = 2) uniform vec2  u_size;
+layout(location = 2) uniform ivec2 u_size;
 
+layout(location = 4) uniform uvec4 u_text[256];
 
 void main(void)
 {
-    vec2 coords = data[gl_VertexID];
-    vec2 pos    = ((vec2(u_position) + coords * u_size) / u_winRes) * 2.0 - 1;
-    pos.y       = pos.y * -1.0;
+    vec2 box = vec2(1.0 / 32.0, 1.0 / 7.0);
+    uint ch = u_text[gl_InstanceID / 16][(gl_InstanceID % 16) / 4];
+    uint n = gl_InstanceID % 4;
+    uint char = ((ch << (24 - 8 * n)) >> 24) - 33;
+
+    vec2 coords  = data[gl_VertexID];
+    ivec2 offset = u_size * ivec2(gl_InstanceID, 0);
+    vec2 pos     = ((vec2(u_position) + coords * u_size + offset) / u_winRes) * 2.0 - 1;
+    pos.y        = pos.y * -1.0;
 
     gl_Position = vec4(pos, 1.0, 1.0);
+
+    vec2 c = vec2(char % 32, char / 32) * box + vec2(coords.x, coords.y) * box;
+    o_coords = vec2(c.x, 1.0 - c.y);
 }
